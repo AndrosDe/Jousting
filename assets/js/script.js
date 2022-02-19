c45fb145aaa06e36a529f756903944b1125cb3d4
AndrosDe <andreas_beier1981@yahoo.de>
2022-02-19 11:04:37 +0000

    corrected some styling and added the countdown in Java Script

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Countdown
+
+let countdown = 10;
+let x = setInterval(function() {
+    if(countdown <= 0) {
+        clearInterval(x)
+    }
+    document.getElementById("countdown").innerHTML = 0 + countdown;
+    countdown -= 1;
+}, 1000);
