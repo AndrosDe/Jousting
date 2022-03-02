316333c10d8b27850e7e309cd0185de2cdaab00a
AndrosDe <andreas_beier1981@yahoo.de>
2022-03-02 10:53:54 +0000

    add java script code for score and random skill generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+// This script should only start once the DOM has finished loading
+/** The start-function gets all the buttons of the DOM and adds event listner to them
+ *  The respective functions are all called by clicking "Start".
+ */
 document.addEventListener ("DOMContentLoaded", function() {
-  let buttons = document.getElementsByTagName ("button");
+  let buttons = document.getElementsByTagName("button");
 
   for (let button of buttons){
     button.addEventListener("click", function(){
@@ -9,9 +13,9 @@ document.addEventListener ("DOMContentLoaded", function() {
         countdown();
         playerMove();
         opponentMove();
+        runGame();
       } else {
-        let skillType = this.getAttribute("data-type");
-        alert(`You clicked ${skillType}`);
+
       }
     })
   }
@@ -35,7 +39,10 @@ function countdown() {
   }, 1000);
 }
   
-// The visusal aid of the animated knight running towards each other to support the countdown
+// The Animation
+/**
+ * Two knights charging another and clash in the middel of the screen
+ */
 function playerMove() {
   document.getElementById("player").style.visibility = "visible";
 
@@ -72,4 +79,68 @@ document.getElementById("opponent").style.visibility = "visible";
         elem.style.left = -pos + 'px';
       }
     }
+}
+
+
+// Main Game Functions
+/**
+ * Function to check which skill button has been clicked
+ * This should then be displayed in the "player-skill"-div and may add a value to the hidden section of the page for comparison
+ */
+function runGame() {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons){
+    button.addEventListener("click", function(){
+      if (this.getAttribute("data-type") === "shield") {
+        document.getElementsByClassName("player-skill").style.background = "url(../images/shield.png)";
+        alert(`You clicked Shield`);
+      } else if (this.getAttribute("data-type") === "lance") {
+        document.getElementsByClassName("player-skill").style.background = "url(../images/lance.png)";
+      } else if (this.getAttribute("data-type") === "ride") {
+        document.getElementsByClassName("player-skill").style.background = "url(../images/horse.png)";
+      } else if (this.getAttribute("data-type") === "") {
+      }
+    }
+  )}
+}
+
+/**
+ * Function to randomly create 1, 2 or 3 to choose the computer picked skill
+ * Function to set up an array of [shield, lance, ride] and assign them an index
+ */
+function opponentSkill(){
+  let skills = [shield, lance, ride]
+  let num = Math.floor(Math.random() *3);
+  
+  if (skills.indexOf("shield") === num ){
+    document.getElementsByClassName("pc-skill").style.background = "url(../images/shield.png)";
+  } else if (skills.indexOf("lance") === num ){
+    document.getElementsByClassName("pc-skill").style.background = "url(../images/lance.png)";
+  } else {
+    document.getElementsByClassName("pc-skill").style.background = "url(../images/horse.png)";
+  }   
+}
+
+/**
+ * Function to compare player skill and computer skill and determine the winner
+ */
+
+
+//Score
+/**
+ * Gets the current Score from the DOM and adds the appropriate value to it
+ * 1000 for a Win
+ * 50 for participation (due to the high randomness and chanceto fail by bad luck)
+ * Only not choosing any skill will award no points
+ */
+
+function winScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = oldScore + 1000;
+}
+
+function loseScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = oldScore + 50;
 }
